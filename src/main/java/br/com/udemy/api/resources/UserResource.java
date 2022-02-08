0544d9f756764384966540fac989a9ac5476b3ab
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2022-02-08 20:14:35 -0300

    classe user resource findById

--- src/main/java/br/com/udemy/api/resources/UserResource.java
+++ src/main/java/br/com/udemy/api/resources/UserResource.java
@@ -1,15 +1,23 @@
 package br.com.udemy.api.resources;
 
 import br.com.udemy.api.domain.User;
+import br.com.udemy.api.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("user")
 public class UserResource {
 
+    private final UserService service;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Jhony", "jhony.o_rodrigues@hotmail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
